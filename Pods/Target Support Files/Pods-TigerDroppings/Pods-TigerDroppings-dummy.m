43e363ef6664d0dc3e2e28f8429768e134c85e6f
James Kiel <jblairkiel@gmail.com>
2022-04-16 15:01:48 -0500

    Before Swift Conversion

---
+++ Pods/Target Support Files/Pods-TigerDroppings/Pods-TigerDroppings-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TigerDroppings : NSObject
+@end
+@implementation PodsDummy_Pods_TigerDroppings
+@end
---
+++ Pods/Target Support Files/Pods-TigerDroppingsTests/Pods-TigerDroppingsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TigerDroppingsTests : NSObject
+@end
+@implementation PodsDummy_Pods_TigerDroppingsTests
+@end
